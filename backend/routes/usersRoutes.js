f4bf608d7df3f4d835d4686dc778b6caf14e2219
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-30 14:11:36 -0400

    creating user routes

---
+++ backend/routes/usersRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import User from '../models/Users.js'
+
+const router = express.Router();
+
+// POST /api/users/register - Create a new user
+
+router.post("/register", async (req,res) => {
+    
+})
