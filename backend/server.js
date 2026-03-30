1dbe8d0f439440c9ec173fb950efb53c7d53495f
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-30 16:23:14 -0400

    imported userRoutes into server.js file

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import "dotenv/config"
 import express from "express"
 import './config/connection.js'
 
-import 
+import userRoutes from './routes/usersRoutes.js'
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Listening to http://localhost:${PORT}`))
