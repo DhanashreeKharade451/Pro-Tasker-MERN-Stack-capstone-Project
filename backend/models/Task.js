446e1d24398201b15abb41d6ac6237a621d58c45
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-30 13:54:39 -0400

    created User schema and Task Schema

---
+++ backend/models/Task.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import Task from "../../../mod-15/TaskMasterApp/models/Task";
+
+const taskSchema = new Schema({
+    title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["To Do", "In Progress", "Done"],
+    default: "To Do",
+  },
+
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
