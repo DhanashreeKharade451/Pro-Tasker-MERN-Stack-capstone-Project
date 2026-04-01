6b8b325b14da6c8e758e42cd3454ee2bf50a52a6
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-01 14:27:23 -0400

    created controller to update  projects and delete  project

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -43,4 +43,39 @@ export const getProject = async (req,res) => {
 
 //update project
 
+export const updateProject = async (req,res) => {
+    try{
+        const project = await Project.findById(req.param.id);
+        if (!project) return res.status(404).json({message: "Project not found"});
 
+        if(project.user.toString() !== req.user._id)
+            return res.status(403).json({message:"Not authorized"});
+
+        const updatedProject =await Project.findByIdAndUpdate(req.param.id, req.body, {
+            new:true
+        });
+        res.status(200).json(updateProject);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
+
+
+//Delete project and its tasks
+export const deleteProject = async (req, res) => {
+    try{
+        const project = await Project.findById(req.param.id);
+        
+        if(!Project) return res.status(404).json({Message: "Project no found"});
+
+        if(project.user.toString() !== res.user._id)
+          return res.status(403).json({ message: "Not authorized" });
+
+        await Project.findByIdAndDelete(req.param.id);
+        await Task.deleteMany({project: req.param.id});
+
+         res.status(200).json({ message: "Project and tasks deleted" });
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
