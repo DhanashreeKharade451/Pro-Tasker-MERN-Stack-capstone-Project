0e49dcfa00a7657772a48c7f5a9d742328141423
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-01 13:29:57 -0400

    created controller to get  projects and get single project

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -21,6 +21,24 @@ export const getProjects = async(req, res) => {
         const projects = await Project.find({user: req.user._id}).populate("user");
         res.status(200).json(projects);
     }catch(err){
-        
+        res.status(500).json({message: err.message})
     }
-}
+};
+
+// GET single project
+
+export const getProject = async (req,res) => {
+    try{
+        const project = await Project.findOne({
+            _id: req.param.id,
+            user:request.user.id,
+        });
+
+        if (!project) return res.status(400).json({message:"project not found"});
+        res.status(200).json(project);
+    }catch(err){
+        res.status(400).json({message: err.message});
+    }
+};
+
+//update project
