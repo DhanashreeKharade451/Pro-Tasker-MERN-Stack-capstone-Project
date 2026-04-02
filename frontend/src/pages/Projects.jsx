7eed74681600583d90a182d8feb937cd29e9b265
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-02 14:09:43 -0400

    created Projects file and added code to handle form and buttons

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,9 +1,81 @@
-function Project(){
-    return(
-        <div>
-            Project Page
-        </div>
-    )
-}
+import { useState,useEffect } from "react";
+import API from "../clients/api"
+import { Link } from "react-router-dom";
 
-export default Project
+function Projects(){
+    const [Projects, setProjects] = useState([]);
+    const[form, setForm] =useState({
+        name: "",
+    description: "",
+    }) ;
+
+    //Fetch all projects
+    const fetchProjects = async() => {
+        try{
+            const res = await API.get("/projects");
+             setProjects(res.data);
+        }catch (err) {
+      console.log({message: err.message});
+    }
+};
+
+ useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  // 🔹 Handle form input
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+   // 🔹 Create project
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await API.post("/projects", form);
+      setForm({ name: "", description: "" });
+      fetchProjects(); // refresh list
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+   // 🔹 Delete project
+  const handleDelete = async (id) => {
+    try {
+      await API.delete(`/projects/${id}`);
+      fetchProjects();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return(
+<div>
+    <h1>Projects</h1>
+
+    {/* create Project Form */}
+    <form onSubmit={handleSubmit}>
+        <input
+        name="name"
+          placeholder="Project Name"
+          value={form.name}
+          onChange={handleChange} />
+
+        <input
+         name="description"
+          placeholder="Description"
+          value={form.description}
+          onChange={handleChange} 
+          />
+          <button> Create Project</button>
+    </form>
+
+</div>
+
+  )
+
+}
