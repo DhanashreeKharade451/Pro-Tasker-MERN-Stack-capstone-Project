232852f7bd511536e1419c4e23907552018db9dc
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-02 09:39:46 -0400

    added a handleChange and State

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
+
 function Register(){
 
-    
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password:'' 
+       })
 
     return(
         <div>
            <h1> Register Page</h1>
            <form>
             <label htmlFor="username">Username: </label>
-            <input id="username" name="username" type="text" required />
+            <input value={form.username} id="username" name="username" type="text" required />
 
             <label htmlFor="email">Email: </label>
-            <input id="email" name="email" type="email" required />
+            <input value={form.email} id="email" name="email" type="email" required />
 
             <label htmlFor="password">Password: </label>
-            <input id="password" name="password" type="password" required />
+            <input value={form.password} id="password" name="password" type="password" required />
 
             <button>Register</button>
            </form>
