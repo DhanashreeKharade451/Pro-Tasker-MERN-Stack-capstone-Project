1a8ff159a5669096b078f838c2f7a4ca3a1f8a53
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-02 15:27:46 -0400

    created dynamic functionality to create task, display task and delete task

--- frontend/src/pages/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage.jsx
@@ -6,12 +6,13 @@ function ProjectPage(){
     const{id} =useParams(); ///project ID from URL
 
     const [project, setProject] = useState(null);
-    const [task, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
     const[form, setForm] = useState({
         title: "",
         description:"",
     });
 
+    // 🔹 Fetch project
     const fetchProject = async () => {
         try {
       const res = await API.get(`/projects/${id}`);
@@ -21,9 +22,55 @@ function ProjectPage(){
     }
     };
 
+     // 🔹 Fetch tasks
+  const fetchTasks = async () => {
+    try {
+      const res = await API.get(`/projects/${id}/tasks`);
+      setTasks(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+//  Load data
    useEffect(() => {
     fetchProject();
+     fetchTasks();
   }, [id]);
+
+
+//handle form input
+const handleChange = (e) => {
+   setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+};
+
+//handle form submit
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post(`/projects/${id}/tasks`, form);
+
+      setForm({ title: "", description: "" });
+
+      fetchTasks(); // refresh
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //handle delete task
+const handleDelete = async (taskId) => {
+    try {
+      await API.delete(`/projects/${id}/tasks/${taskId}`);
+      fetchTasks();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   if (!project) return <p>Loading...</p>;
 
@@ -34,6 +81,42 @@ function ProjectPage(){
 
       {/* Tasks will go here */}
       <h2>Tasks</h2>
+
+       {/* 🔹 Create Task Form */}
+       <form  onSubmit={handleSubmit}>
+        <input
+          name="title"
+          placeholder="Task Title"
+          value={form.title}
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="description"
+          placeholder="Description"
+          value={form.description}
+          onChange={handleChange}
+          required
+        />
+        <button>Add Task</button>
+       </form>
+
+       {/* Display TAsk */}
+       {tasks.length === 0 ? (
+        <p>No tasks yet</p>
+      ) : (
+        tasks.map((task) => (
+          <div key={task._id}>
+            <h4>{task.title}</h4>
+            <p>{task.description}</p>
+            <p>Status: {task.status}</p>
+
+            <button onClick={() => handleDelete(task._id)}>
+              Delete
+            </button>
+          </div>
+        ))
+      )}
     </div>
   )
 
