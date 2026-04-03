671983f076a120ad4d92db951c5fa4691dc229f1
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-03 01:18:39 -0400

    careating Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+
+
 function Dashboard(){
+    const {user} = useUser();
+
+    const [projects, setProjects] = useState([]);
+    const[Loading, setloading] = useState(true);
+
+    //fetch Projects
+const fetchProject = async () => {
+    try{
+        const res = await API.get("/projects");
+        setProjects(res.data);
+    }catch(err){
+        console.log(err);
+    }finally{
+        setloading(false);
+    }
+};
+
+useEffect(() => {
+    fetchProject();
+},[]);
+
     return(
-        <div>
-            Dashboard Page
+        <div style={styles.container}>
+
+            {/* header */}
+            <h1 style={styles.heading}>Welcome, {user?.username || "User"}</h1>
+
+            {/* stats section */}
+
+            <div>
+                <div>
+                    <h2>{projects.length}</h2>
+                    <p>Total Projects</p>
+                </div>
+            </div>
         </div>
     )
 }
