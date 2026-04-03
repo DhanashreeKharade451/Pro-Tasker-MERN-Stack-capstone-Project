f9ad62f8495d89a0b2a5759ecfcbc4243a6ed15d
Dhanashree <dhanashreeakharade@gmail.com>
2026-04-03 15:07:15 -0400

    Added message in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.use(cors({origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173' ]}));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes)
 app.use("/api/projects", taskRoutes)
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
 
 app.listen(PORT, () => console.log(`Listening to http://localhost:${PORT}`))
